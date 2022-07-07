bc172a64f54e2c0a8f1a3a1fae8c5ef1607bcde2
bellmann-engineering <kai@doomcom.de>
2022-07-07 20:35:25 +0200

    added README, LICENSE, and two patterns

---
+++ Adapter/demo.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+// a class containing only pure virtual methods ≙ interface
+class ThermoInterface
+{
+    public:
+        virtual double getTempC() = 0;
+};
+
+// Lecacy component
+class CelciusThermo : public ThermoInterface
+{
+    public:
+        double getTempC() 
+        {
+            return -20;
+        }
+    
+};
+
+class FahrenheitThermo
+{
+    public:
+        double getFahrenheitTemperature() 
+        {
+            return -4; // -4 °F should be -20° C
+        }
+    
+};
+
+class ThermoAdapter : public ThermoInterface
+{
+    private:
+        FahrenheitThermo* thermo;
+    public:
+        ThermoAdapter() 
+        {
+                thermo = new FahrenheitThermo();
+        }
+        ~ThermoAdapter() 
+        {
+                if(thermo != nullptr)
+                {
+                    delete thermo;
+                    thermo = nullptr;
+                }
+        }
+        double getTempC()
+        {
+                return (thermo->getFahrenheitTemperature()-32.0) * (5.0/9.0);
+        }
+};
+
+int main()
+{
+    ThermoInterface* ti = new ThermoAdapter();
+    std::cout << ti->getTempC() << std::endl;
+
+    if(ti != nullptr)
+    {
+        delete ti;
+        ti = nullptr;
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Content
+
+Hands-On Software Architecture Patterns with C++ by Kai Bellmann
+
+Each folder contains sample code for the following design patterns:
+- Adapter
+- Bridge
